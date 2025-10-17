d76abc8b59ec2e95013e6a4037669f852fb1d637
Minh Nhi <minhnhi15102003@gmail.com>
2025-10-17 12:02:41 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
-import SignUp from "./pages/AuthenPages/SignUp";
-import SignIn from "./pages/AuthenPages/SignIn";
-import Product from "./pages/ProductManagement/Product";
 import Home from "./pages/Dashboard/Home";
 import { ListProduct } from "./pages/ProductManagement/ListProduct";
+import Product from "./pages/ProductManagement/Product";
 import ListCategory from "./pages/Category/ListCategory";
+import SignUp from "./pages/AuthenPages/SignUp";
+import SignIn from "./pages/AuthenPages/SignIn";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/project-admin">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<AppLayout />}>
           <Route index element={<Home />} />
@@ -21,6 +20,6 @@ export default function App() {
         <Route path="signup" element={<SignUp />} />
         <Route path="signin" element={<SignIn />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
