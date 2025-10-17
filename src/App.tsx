56e2e6d4253c495eb85226bfcf38b2f15e33e12e
Minh Nhi <minhnhi15102003@gmail.com>
2025-10-17 11:57:57 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
-import {BrowserRouter,Route, Routes} from "react-router-dom"
-import './App.css'
-import AppLayout from "./layout/AppLayout"
-import SignUp from "./pages/AuthenPages/SignUp"
-import SignIn from "./pages/AuthenPages/SignIn"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import AppLayout from "./layout/AppLayout";
+import SignUp from "./pages/AuthenPages/SignUp";
+import SignIn from "./pages/AuthenPages/SignIn";
 import Product from "./pages/ProductManagement/Product";
-import Home from "./pages/Dashboard/Home"
-import { ListProduct } from "./pages/ProductManagement/ListProduct"
-import ListCategory from "./pages/Category/ListCategory"
+import Home from "./pages/Dashboard/Home";
+import { ListProduct } from "./pages/ProductManagement/ListProduct";
+import ListCategory from "./pages/Category/ListCategory";
 
-function App() {
-
+export default function App() {
   return (
-    <>
-      <BrowserRouter basename="/project-admin">
-        <Routes>
-          <Route path="/" element={<AppLayout/>}>
-            <Route path="" element={<Home/>}/>
-            <Route path="list-product" element={<ListProduct/>}/>
-            <Route path="product" element={<Product/>}/>
-            <Route path="category" element={<ListCategory/>}/>
+    <BrowserRouter basename="/project-admin">
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="list-product" element={<ListProduct />} />
+          <Route path="product" element={<Product />} />
+          <Route path="category" element={<ListCategory />} />
         </Route>
-        <Route path="signup" element={<SignUp/>}/>
-        <Route path="signin" element={<SignIn/>}/>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
